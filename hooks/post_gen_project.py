3f5cd2e903411bc9a11d4062a547b9dc9d41891c
Brett Copeland <brcopeland@gmail.com>
2021-08-04 14:10:27 -0700

    Added hook to specify profile path in config.yaml.

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,14 @@
+import os
+target = os.path.join(os.getcwd(), "{{cookiecutter.profile_name}}")
+
+# replaces all occurrences of $((INSTALL)) with the full path of the directory
+# installed to
+for root, dirs, fns in os.walk(target):
+    for fn in fns:
+        fn = os.path.join(root, fn)
+        with open(fn) as fh:
+            content = fh.read()
+        content = content.replace("$((INSTALL))", target)
+        with open(fn, "w") as fh:
+            f.write(content)
+
