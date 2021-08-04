7871b4670418082bfc6c9726e4a14cc58629eac9
Brett Copeland <brcopeland@gmail.com>
2021-08-04 14:20:59 -0700

    Fixed getting project directory.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,13 +1,11 @@
 import os
-target = os.path.join(os.getcwd(), "{{cookiecutter.profile_name}}")
-print(target)
+os.getcwd()
 
 # replaces all occurrences of $((INSTALL)) with the full path of the directory
 # installed to
 for root, dirs, fns in os.walk(target):
     for fn in fns:
         fn = os.path.join(root, fn)
-        print(f"checking {fn}")
         with open(fn) as fh:
             content = fh.read()
         content = content.replace("$((INSTALL))", target)
