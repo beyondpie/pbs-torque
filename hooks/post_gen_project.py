6fe04e3b54525fcc938d936fa72aa29a7439f51b
Brett Copeland <brcopeland@gmail.com>
2021-08-26 13:50:43 -0700

    Fixed getting path to config.yaml.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,10 +3,10 @@ target = os.getcwd()
 
 # replace $((USER)) in config file with the user name
 user = os.getlogin()
-with open("config/config.yaml") as config_fh:
+with open("config.yaml") as config_fh:
     config_content = config_fh.read()
 config_content = config_content.replace("$((USER))", user)
-with open("config/config.yaml", "w") as config_fh:
+with open("config.yaml", "w") as config_fh:
     config_fh.write(config_content)
 
 # replaces all occurrences of $((INSTALL)) with the full path of the directory
