3ad8193b0aad190d8cbc78b3ff274297d3cebffa
Brett Copeland <brcopeland@gmail.com>
2021-08-26 13:48:19 -0700

    Added more to config.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,6 +1,14 @@
 import os
 target = os.getcwd()
 
+# replace $((USER)) in config file with the user name
+user = os.getlogin()
+with open("config/config.yaml") as config_fh:
+    config_content = config_fh.read()
+config_content = config_content.replace("$((USER))", user)
+with open("config/config.yaml", "w") as config_fh:
+    config_fh.write(config_content)
+
 # replaces all occurrences of $((INSTALL)) with the full path of the directory
 # installed to
 for root, dirs, fns in os.walk(target):
---
+++ hooks/pre_gen_project.py
@@ -0,0 +1,19 @@
+import sys
+
+restart_times = {{cookiecutter.restart_times}}
+try:
+    restart_times = int(restart_times)
+    if restart_times < 0:
+        raise ValueError
+except ValueError:
+    print(f"restart_times ({restart_times}) must be a non-negative integer.")
+    sys.exit(1)
+
+latency_wait = {{cookiecutter.latency_wait}}
+try:
+    latency_wait = float(latency_wait)
+    if latency_wait < 0:
+        raise ValueError
+except ValueError:
+    print(f"latency_wait ({latency_wait}) must be a non-negative float.")
+    sys.exit(1)
