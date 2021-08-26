6250ef784d68e4d7dd4826506146d7e848a17cf3
Brett Copeland <brcopeland@gmail.com>
2021-08-26 13:52:33 -0700

    Fixed checking numeric values.

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,6 +1,6 @@
 import sys
 
-restart_times = {{cookiecutter.restart_times}}
+restart_times = "{{cookiecutter.restart_times}}"
 try:
     restart_times = int(restart_times)
     if restart_times < 0:
@@ -9,7 +9,7 @@ except ValueError:
     print(f"restart_times ({restart_times}) must be a non-negative integer.")
     sys.exit(1)
 
-latency_wait = {{cookiecutter.latency_wait}}
+latency_wait = "{{cookiecutter.latency_wait}}"
 try:
     latency_wait = float(latency_wait)
     if latency_wait < 0:
